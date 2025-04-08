fd1ab6daaeb9568fb8663f4467c70bbae2bf05cd
samuel.victor <samuel.victor@ebserh.gov.br>
2025-04-08 02:20:18 -0300

    � Redirects netlify

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,8 +12,6 @@ const routes = [
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
